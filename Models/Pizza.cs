d0b713adbf6a5a25f1bb0ca9bbb74899de6999b5
Carlos Yaben <carlosyaben@gmail.com>
2024-01-20 15:38:42 -0300

    Agregar archivos de proyecto.

---
+++ Models/Pizza.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductAPI.Models
+{
+    public class Pizza
+    {
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsGlutenFree { get; set; }
+
+    }
+}
